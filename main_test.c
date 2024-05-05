cc3ee4d093b930e45696b94473d13011dcfa41b4
budindepunk <carolinasilvarode@gmail.com>
2024-05-05 20:10:13 +0200

    tests for undefined behavior

--- main_test.c
+++ main_test.c
@@ -30,50 +30,61 @@ int main(void)
     int hex = 1500;
 
     // string    
-    printf("+++ s +++\n");
+    printf("\n+++ s +++\n");
     printf(" || OG printf returned: %d\n", printf("%s", string));
     printf(" || MY printf returned: %d\n", ft_printf("%s", string));
+    
     // char
-    printf("+++ c +++\n");
+    printf("\n+++ c +++\n");
     printf(" || OG printf returned: %d\n", printf("%c", charcito));
     printf(" || MY printf returned: %d\n", ft_printf("%c", charcito));
+    
     // int
-    printf("+++ i +++\n");
+    printf("\n+++ i +++\n");
     printf(" || OG printf returned: %d\n", printf("%i", i));
     printf(" || MY printf returned: %d\n", ft_printf("%i", i));
+    
     // dec
-    printf("+++ d +++\n");
+    printf("\n+++ d +++\n");
     printf(" || OG printf returned: %d\n", printf("%d", d));
     printf(" || MY printf returned: %d\n", ft_printf("%d", d));
+    
     // unsigned int
-    printf("+++ u +++\n");
+    printf("\n+++ u +++\n");
     printf(" || OG printf returned: %d\n", printf("%u", ui_pos));
     printf(" || MY printf returned: %d\n", ft_printf("%u", ui_pos));
     printf(" || OG printf returned: %d\n", printf("%u", ui_neg));
     printf(" || MY printf returned: %d\n", ft_printf("%u", ui_neg));
+    
     // hex lower
-    printf("+++ x +++\n");
+    printf("\n+++ x +++\n");
     printf(" || OG printf returned: %d\n", printf("%x", hex));
     printf(" || MY printf returned: %d\n", ft_printf("%x", hex));
+    
     // hex upper
-    printf("+++ X +++\n");
+    printf("\n+++ X +++\n");
     printf(" || OG printf returned: %d\n", printf("%X", hex));
     printf(" || MY printf returned: %d\n", ft_printf("%X", hex));
+    
     // pointer
-    printf("+++ p +++\n");
+    printf("\n+++ p +++\n");
     printf(" || OG with p: %d\n", printf("%p", &hex));
     printf(" || MY with p: %d\n", ft_printf("%p", &hex));
+    
     // %
-    printf("+++ %% +++\n");
+    printf("\n+++ %% +++\n");
     printf(" || OG printf returned: %d\n", printf("%%"));
     printf(" || MY printf returned: %d\n", ft_printf("%%"));
+    
     // bunch of things at once
-    printf("+++ bunch-o-things +++\n");
+    printf("\n+++ bunch-o-things +++\n");
     printf(" || OG printf returned: %d\n", printf("hola esto es una string: %s y esto es un puntero: %p", string, &string));
     printf(" || MY printf returned: %d\n", ft_printf("hola esto es una string: %s y esto es un puntero: %p", string, &string));
     printf(" || OG printf returned: %d\n", printf("%s, %c, %d, %i, %x, %p", string, charcito, d, i, hex, &hex));
     printf(" || MY printf returned: %d\n", ft_printf("%s, %c, %d, %i, %x, %p", string, charcito, d, i, hex, &hex));
 
+    // ??
+    printf("\n+++ no arguments ?? undefined behavior +++\n");
     printf(" || OG printf returned: %d\n", ft_printf("esta es la fallida: %s"));
     
     return(0);
